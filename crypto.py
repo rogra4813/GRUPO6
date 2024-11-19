1ff4b5954061b320a23577dfd00b4b3a67cdba1e
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-19 14:01:12 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -10,7 +10,7 @@ st.text("***********************************************************************
 st.text("Web scrapping al URL https://es.investing.com/crypto")
 st.text("Integrantes: Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar")
 try:
-df = pd.read_excel("datos_criptomonedas.csv")
+df = pd.read_csv("datos_criptomonedas.csv")
 st.write(df)
 except FileNotFoundError:
     st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
