561b07ec58ca8a314e69cdd84e8e98714a7ceba4
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-19 16:10:43 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -59,7 +59,7 @@ def load_data():
     # Extract cryptocurrency data
     cryptocurrencies = data['data']
 
-    coin_name = []
+    Nombre = []
     coin_symbol = []
     market_cap = []
     percent_change_1h = []
@@ -69,7 +69,7 @@ def load_data():
     volume_24h = []
 
     for crypto in cryptocurrencies:
-        coin_name.append(crypto['name'])
+        Nombre.append(crypto['name'])
         coin_symbol.append(crypto['symbol'])
         price.append(crypto['quote'][currency_price_unit]['price'])
         percent_change_1h.append(crypto['quote'][currency_price_unit]['percent_change_1h'])
@@ -79,9 +79,9 @@ def load_data():
         volume_24h.append(crypto['quote'][currency_price_unit]['volume_24h'])
 
     df = pd.DataFrame(
-        columns=['coin_name', 'coin_symbol', 'price', 'marketCap', 'percentChange1h', 'percentChange24h', 'percentChange7d',
+        columns=['Nombre', 'coin_symbol', 'price', 'marketCap', 'percentChange1h', 'percentChange24h', 'percentChange7d',
                  'volume24h'])
-    df['coin_name'] = coin_name
+    df['Nombre'] = Nombre
     df['coin_symbol'] = coin_symbol
     df['price'] = price
     df['percentChange1h'] = percent_change_1h
