ef3f7cd2cbe56094f3478c4686ef83d7c623966e
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-19 10:41:20 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+import base64
 import io
+import os
 
 st.title('EJERCICIO PRÁCTICO GRUPO 6')
 st.text("*************************************************************************************************************")
@@ -8,10 +11,16 @@ st.text("Web scrapping al URL https://es.investing.com/crypto")
 st.text("Integrantes: Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar")
 #try:
 #df = pd.read_excel("datos_criptomonedas.csv")
-st.write(df)
+#st.write(df)
 #except FileNotFoundError:
     #st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
 #except Exception as e:
     #st.error(f"Ocurrió un error al cargar el archivo: {e}")
+# Load the data
+def load_data():
+    df = pd.read_csv("datos_criptomonedas.csv")
+    return df
+
+data = load_data().copy()
 
 
