d546f3eedd09838517977ce880d9281c2740c0bb
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 22:52:30 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -5,11 +5,11 @@ st.title('EJERCICIO PRÁCTICO GRUPO 6')
 st.text("*************************************************************************************************************")
 st.text("Web scrapping al URL https://es.investing.com/crypto")
 st.text("Integrantes: Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar")
-try:
+#try:
    df = pd.read_excel("Presupuesto personal1.xlsx")
    st.write(df)
-except FileNotFoundError:
-    st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
-except Exception as e:
-    st.error(f"Ocurrió un error al cargar el archivo: {e}")
+#except FileNotFoundError:
+    #st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
+#except Exception as e:
+    #st.error(f"Ocurrió un error al cargar el archivo: {e}")
 
