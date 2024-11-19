955f9ea6b0f1593b90f2599c5fa05b799d63fa0a
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 20:53:30 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-import pip
-pip.main(["install", "openpyxl"])
+#import pip
+#pip.main(["install", "openpyxl"])
 
 st.title('EJERCICIO PRÁCTICO GRUPO 6')
 
-df = df.read_csv('precios_criptomonedas_incrementales.csv')
+#df = df.read_csv('precios_criptomonedas_incrementales.csv')
 
-df.wrtite(df)
+#df.wrtite(df)
+
+with open('precios_criptomonedas_incrementales.csv', mode='r') as archivo_csv:
+    lector_csv = csv.reader(archivo_csv)
+    for fila in lector_csv:
+        print(fila)
