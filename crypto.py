aa88934392f2fbe847d34970445e01af025884b0
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-19 14:03:35 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -9,6 +9,15 @@ st.title('EJERCICIO PRÁCTICO GRUPO 6')
 st.text("*************************************************************************************************************")
 st.text("Web scrapping al URL https://es.investing.com/crypto")
 st.text("Integrantes: Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar")
+# ---------------------------------#
+# About
+expander_bar = st.expander("**About**")
+expander_bar.markdown("""
+*:orange[Usage:]* Este ejercicio práctico permite consolidar lo aprendido en Procesos ETL.
+
+*:orange[Created By:]* :blue[Aditya Singh Amber]  from the B.C.A course at Jeevandeep Mahavidyalya.
+""")
+
 try:
 df = pd.read_csv("datos_criptomonedas.csv")
 st.write(df)
