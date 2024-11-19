4d4cfe6660088af3e466ad786b1aae4a2874f3ff
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 22:20:06 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -6,13 +6,9 @@ import pandas as pd
 st.title('EJERCICIO PRÁCTICO GRUPO 6')
 try:
    df = pd.read_excel('cryptos.xlsx')
-   st.write()
+   st.write(df)
 except FileNotFoundError:
     st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
 except Exception as e:
     st.error(f"Ocurrió un error al cargar el archivo: {e}")
 
-#with open('precios_criptomonedas_incrementales.csv', mode='r') as archivo_csv:
-   # lector_csv = csv.reader(archivo_csv)
-   # for fila in lector_csv:
-       # print(fila)
