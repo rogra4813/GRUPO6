2e7486a73d88c2083b9ff63f2d6d4d8864d1c4e2
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 20:45:44 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-st.title('GRUPO 6')
+st.title('EJERCICIO PRÁCTICO GRUPO 6')
+
+df = df.read.csv('precios_criptomonedas_incrementales.csv')
+
+df.wrtite(df)
