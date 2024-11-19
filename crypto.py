c5024d9232000c73148b5755c7f7758fa2ec94c4
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 23:04:10 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -4,23 +4,24 @@
 
 #pip.main(["install" , "])
 
-#st.title('EJERCICIO PRÁCTICO GRUPO 6')
-#st.text("*************************************************************************************************************")
-#st.text("Web scrapping al URL https://es.investing.com/crypto")
-#st.text("Integrantes: Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar")
-#try:
-#df = pd.read_excel("Presupuesto personal1.xlsx")
-#st.write(df)
-#except FileNotFoundError:
-    #st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
-#except Exception as e:
-    #st.error(f"Ocurrió un error al cargar el archivo: {e}")
 from datetime import datetime, timedelta
 
 import pandas as pd
 import streamlit as st
 from mitosheet.streamlit.v1 import spreadsheet
 from mitosheet.streamlit.v1.spreadsheet import _get_mito_backend
+st.title('EJERCICIO PRÁCTICO GRUPO 6')
+st.text("*************************************************************************************************************")
+st.text("Web scrapping al URL https://es.investing.com/crypto")
+st.text("Integrantes: Robert Granda, Francisco García, Fabián Quito y Gabriel Salazar")
+#try:
+#df = pd.read_excel("Presupuesto personal1.xlsx")
+st.write(df)
+#except FileNotFoundError:
+    #st.error("El archivo 'cryptos.xlsx' no se encuentra en el directorio.")
+#except Exception as e:
+    #st.error(f"Ocurrió un error al cargar el archivo: {e}")
+
 
 st.set_page_config(layout="wide")
 
