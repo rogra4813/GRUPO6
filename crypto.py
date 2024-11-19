9943dd9576b15df79c9ec9a3f38a83149c560363
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-19 14:26:15 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -94,11 +94,7 @@ def load_data():
 
 df = load_data()
 
-## Sidebar - Cryptocurrency selections
-sorted_coin = sorted(df['coin_symbol'])
-selected_coin = col1.multiselect('Cryptocurrency', sorted_coin, sorted_coin)
 
-df_selected_coin = df[df['coin_symbol'].isin(selected_coin)]  # Filtering data
 
 ## Sidebar - Number of coins to display
 num_coin = col1.slider('Display Top N Coins', 1, 100, 100)
