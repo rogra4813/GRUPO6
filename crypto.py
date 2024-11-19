0e7b39cf5cd9c2c731c2036f0f3dcd137deab4f5
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-19 14:48:47 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -68,9 +68,8 @@ def load_data():
     price = []
     volume_24h = []
 
-
     for crypto in cryptocurrencies:
-        coin_name.append(crypto['Name'])
+        coin_name.append(crypto['name'])
         coin_symbol.append(crypto['symbol'])
         price.append(crypto['quote'][currency_price_unit]['price'])
         percent_change_1h.append(crypto['quote'][currency_price_unit]['percent_change_1h'])
@@ -79,7 +78,7 @@ def load_data():
         market_cap.append(crypto['quote'][currency_price_unit]['market_cap'])
         volume_24h.append(crypto['quote'][currency_price_unit]['volume_24h'])
 
-     df = pd.DataFrame(
+    df = pd.DataFrame(
         columns=['coin_name', 'coin_symbol', 'marketCap', 'percentChange1h', 'percentChange24h', 'percentChange7d',
                  'price', 'volume24h'])
     df['coin_name'] = coin_name
