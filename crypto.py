3b9fa1a74c8c3ed5aca9e108a861c6c0eb465141
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 22:05:36 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 import pip
-pip.main(["install","openpyxl"])
+pip.main(["install" , "openpyxl"])
 
 st.title('EJERCICIO PRÁCTICO GRUPO 6')
 
-df = pd.read_excel('GRUPO6/cryptos.xlsx')
+df = pd.read_excel('cryptos.xlsx')
 
 st.write(df)
 
