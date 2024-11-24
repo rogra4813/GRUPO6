129150d98460a8e392e631d8c3655ce9b6433e55
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-23 20:15:13 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -4,6 +4,7 @@ import streamlit as st
 #from PIL import Image
 import pandas as pd
 #import matplotlib.pyplot as plt
+import plotly.express as px
 import requests
 
 
@@ -114,16 +115,11 @@ sort_values = col1.selectbox('Ordenar?', ['Si', 'No'])
 col2.subheader('10 CRIPTOMONEDAS MÁS POPULARES')
 col2.dataframe(df_coins)
 
-# ---------------------------------#
 # Gráfica de precios de criptomonedas seleccionadas en el contenedor principal (col2)
 with col2:
     if not df_coins.empty:
-        plt.figure(figsize=(10, 5))
-        plt.bar(df_coins['Nombre'], df_coins['PrecioUSD'], color='blue')
-        plt.xlabel('Criptomonedas')
-        plt.ylabel(f'Precio en {currency_price_unit}')
-        plt.title('Precios de Criptomonedas Seleccionadas')
-        plt.xticks(rotation=45)
+        fig = px.bar(df_coins, x='Nombre', y='PrecioUSD', title='Precios de Criptomonedas Seleccionadas',
+                     labels={'Nombre': 'Criptomonedas', 'PrecioUSD': f'Precio en {currency_price_unit}'})
         
         # Mostrar la gráfica en Streamlit
-        st.pyplot(plt)
+        st.plotly_chart(fig)
